017298c237d1751ce768896aa3c1a7e0ec5f4f51
CoderShawon <shawonb500@gmail.com>
2023-10-31 21:18:17 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+// All gallery images
+export default[
+    {
+        id: 1,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+    },
+
+]
