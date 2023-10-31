749db19647a60e7cbe7b563e9a148a286145f98b
CoderShawon <shawonb500@gmail.com>
2023-10-31 23:11:35 +0600

    image reordering system done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,100 @@
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
 
-import './App.css'
+import galleryList from "./data.js";  // imported data.js file
 
-function App() {
+const Card = ({ src, title, id, index, moveImage }) => {
+  const ref = React.useRef(null);
+
+  const [, drop] = useDrop({
+    accept: "image",
+    hover: (item, monitor) => {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      moveImage(dragIndex, hoverIndex);
+
+      item.index = hoverIndex;
+    }
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "image",
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => {
+      return {
+        isDragging: monitor.isDragging()
+      };
+    }
+  });
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div ref={ref} style={{ opacity }} className="card border-b-gray-400 border-1">
+      <img src={src} alt={title} />
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  const [images, setImages] = React.useState(galleryList);
+
+  const moveImage = React.useCallback((dragIndex, hoverIndex) => {
+    setImages((prevCards) => {
+      const clonedCards = [...prevCards];
+      const removedItem = clonedCards.splice(dragIndex, 1)[0];
+
+      clonedCards.splice(hoverIndex, 0, removedItem);
+      return clonedCards;
+    });
+  }, []);
+  return (
+    /*------------ Image Gallery section start-------------*/
+    <div className="container">
+    <h1 className="text-center text-[#197685] block uppercase mb-2 text-3xl font-semibold font-mono mt-10">---Gallery---</h1>
+    <main className="">
+      {React.Children.toArray(
+        images.map((image, index) => (
+          <Card
+            src={image.img}
+            title={image.title}
+            id={image.id}
+            index={index}
+            moveImage={moveImage}
+          />
+        ))
+      )}
+    </main>
+    </div>
+    /*------------ Image Gallery section end-------------*/
+  );
+};
+
+export default App;
--- src/data.js
+++ src/data.js
@@ -6,27 +6,43 @@ export default[
     },
     {
         id: 2,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/TB97Kfg/image-2.webp"
     },
     {
         id: 3,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/m800Q9R/image-3.webp"
     },
     {
         id: 4,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/dLx41NH/image-4.webp"
     },
     {
         id: 5,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/FmdpybW/image-5.webp"
     },
     {
         id: 6,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/jDzgz89/image-6.webp"
     },
     {
         id: 7,
-        img: "https://i.ibb.co/XJrJWRc/image-1.webp"
+        img: "https://i.ibb.co/5FGScv4/image-7.webp"
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/nswBDSb/image-8.webp"
+    },
+    {
+        id: 9,
+        img: "https://i.ibb.co/9s7g7LZ/image-9.webp"
+    },
+    {
+        id: 10,
+        img: "https://i.ibb.co/NL5MgFd/image-10.jpg"
+    },
+    {
+        id: 11,
+        img: "https://i.ibb.co/fQpvMzD/image-11.jpg"
     },
 
 ]
