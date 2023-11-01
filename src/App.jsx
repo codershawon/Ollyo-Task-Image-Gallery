6c625d6ce527c40c24d54950c7b19e9bd81d8a11
CoderShawon <shawonb500@gmail.com>
2023-11-01 11:21:15 +0600

    checkbox implimentation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
 
 import galleryList from "./data.js";  // imported data.js file
-import NewImages from "./components/NewImages.jsx";
+import NewImages from "./components/NewImages.jsx"; //imported NewImages.jsx file
 
-const Card = ({ src, title, id, index, moveImage,isFeature }) => {
+const Card = ({ src, title, id, index, moveImage, isFeature, selectedImages, toggleImageSelection }) => {
   const ref = React.useRef(null);
   const [, drop] = useDrop({
     accept: "image",
@@ -20,7 +20,6 @@ const Card = ({ src, title, id, index, moveImage,isFeature }) => {
       }
 
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
-
       const hoverMiddleY =
         (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
@@ -54,18 +53,30 @@ const Card = ({ src, title, id, index, moveImage,isFeature }) => {
   });
 
   const opacity = isDragging ? 0 : 1;
-  drag(drop(ref));
 
   return (
-    <div ref={ref}  className={`card border-b-gray-400 border-1 ${isFeature ? 'featured' : ''}`}
-    style={{ opacity }}>
-      <img src={src} alt={title} />
+    <div
+      ref={ref}
+      className={`card border-b-gray-400 border-1 ${isFeature ? 'featured' : ''}`}
+      style={{ opacity }}
+    >
+      {/* Container for image and checkbox */}
+      <div className="card-content">
+        <img src={src} alt={title} />
+        {/* Checkbox - Hidden by default and shown on hover */}
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={selectedImages.includes(id)}
+          onChange={() => toggleImageSelection(id)}
+        />
+      </div>
     </div>
   );
 };
 const App = () => {
   const [images, setImages] = React.useState(galleryList);
-
+  const [selectedImages, setSelectedImages] = useState([]);
   const moveImage = React.useCallback((dragIndex, hoverIndex) => {
     setImages((prevCards) => {
       const clonedCards = [...prevCards];
@@ -75,11 +86,25 @@ const App = () => {
       return clonedCards;
     });
   }, []);
+
+  const toggleImageSelection = (imageId) => {
+    if (selectedImages.includes(imageId)) {
+      setSelectedImages(selectedImages.filter((id) => id !== imageId));
+    } else {
+      setSelectedImages([...selectedImages, imageId]);
+    }
+  };
   return (
     /*------------ Image Gallery section start-------------*/
     <div className="container">
     <h1 className="text-center text-[#197685] block uppercase mb-2 text-3xl font-semibold font-mono mt-10">---Gallery---</h1>
-    <main className="">
+  {/* Total selected images */}
+     {selectedImages.length > 0 && (
+        <div className="selected-images-info">
+          {selectedImages.length} {selectedImages.length === 1 ? "file" : "files"} selected
+        </div>
+      )}
+    <main className="image-gallery">
       {React.Children.toArray(
         images.map((image, index) => (
           <Card
@@ -89,6 +114,8 @@ const App = () => {
             index={index}
             moveImage={moveImage}
             isFeature={index === 0} // Make the first image is feature image
+            selectedImages={selectedImages}
+            toggleImageSelection={toggleImageSelection}
           />
         ))
       )}
